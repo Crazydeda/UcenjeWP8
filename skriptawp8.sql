82a261896e04eeed01b7f929c6592cb5190226b5
Dražen Dujak <dujak.drazen@gmail.com>
2025-05-09 18:27:39 +0200

    kreiranje tablice grupe

--- skriptawp8.sql
+++ skriptawp8.sql
@@ -4,10 +4,18 @@
 
 --use edunovawp8;
 
-create table smjerovi(
+--create table smjerovi(
+--sifra int,
+--naziv varchar(50),
+--cijena decimal(18,2),
+--datumpokretanja datetime,
+--aktivan bit
+--);
+
+
+create table grupe(
 sifra int,
-naziv varchar(50),
-cijena decimal(18,2),
-datumpokretanja datetime,
-aktivan bit
+naziv varchar(20),
+smjer int,
+predavac varchar(50),
 );
