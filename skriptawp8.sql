64b557f566e9075737d1e22cde8162ae419d2e3f
Dražen Dujak <dujak.drazen@gmail.com>
2025-05-09 18:06:35 +0200

    kreiranje tablice smjerovi

--- skriptawp8.sql
+++ skriptawp8.sql
@@ -2,4 +2,12 @@
 
 --drop database edunovawp8;
 
-use edunovawp8;
+--use edunovawp8;
+
+create table smjerovi(
+sifra int,
+naziv varchar(50),
+cijena decimal(18,2),
+datumpokretanja datetime,
+aktivan bit
+);
