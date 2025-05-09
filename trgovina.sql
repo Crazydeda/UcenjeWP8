89bdd2dc84cb90fc1940a138023619bd73569f6e
Dražen Dujak <dujak.drazen@gmail.com>
2025-05-09 19:15:00 +0200

    Create trgovina.sql

---
+++ trgovina.sql
@@ -0,0 +1,22 @@
+﻿--create database trgovina;
+
+use trgovina;
+
+create table racuni(
+sifra int,
+broj varchar(10),
+kupac varchar(50)
+);
+
+
+create table proizvodi(
+sifra int,
+naziv varchar(20),
+cijena decimal(10,2)
+);
+
+create table stavke(
+racun int,
+proizvod int,
+kolicina decimal(10,2)
+);
