41d8231aace2b0b87ccc7449547e114bf83f39ea
Dražen Dujak <dujak.drazen@gmail.com>
2025-05-09 18:36:24 +0200

    kreiranje grupe clanovi

--- skriptawp8.sql
+++ skriptawp8.sql
@@ -27,11 +27,11 @@
 --email varchar(100)
 --);
 
-create table polaznici(
-sifra int,
-ime varchar(50),
-prezime varchar(50),
-email varchar(100)
+create table clanovi(
+grupa int,
+polaznik int
 );
+
+
 
 
